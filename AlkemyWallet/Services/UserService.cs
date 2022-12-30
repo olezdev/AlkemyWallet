2db442f8c170239e6995bfb2bf4c6b276a71f3e8
Olivera Leonardo <oliveraleonardoezequiel@gmail.com>
2022-12-30 20:10:12 -0300

    Add UserServiceGetAll

---
+++ AlkemyWallet/Services/UserService.cs
@@ -0,0 +1,23 @@
+﻿using AlkemyWallet.Core.Models.DTO;
+using AlkemyWallet.Repositories.Interfaces;
+using AlkemyWallet.Services.Interfaces;
+using AutoMapper;
+
+namespace AlkemyWallet.Services;
+
+public class UserService : IUserService
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+    public UserService(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<List<UserDTO>> GetAllAsync()
+    {
+        var users = await _unitOfWork.UserRepository.GetAllAsync();
+        return _mapper.Map<List<UserDTO>>(users);
+    }
+}
