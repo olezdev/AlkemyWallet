1313b5ab093a29add1d2340626a90dba60cb2241
Olivera Leonardo <oliveraleonardoezequiel@gmail.com>
2022-12-30 20:13:47 -0300

    Add UsersControllerGetAll

---
+++ AlkemyWallet/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using AlkemyWallet.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlkemyWallet.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UsersController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        return Ok(await _userService.GetAllAsync());
+    }
+
+}
