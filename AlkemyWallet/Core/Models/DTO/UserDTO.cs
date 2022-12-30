b7119818174dc7b697e997685726da041f611064
Olivera Leonardo <oliveraleonardoezequiel@gmail.com>
2022-12-30 20:10:32 -0300

    Add MapperUsers

---
+++ AlkemyWallet/Core/Models/DTO/UserDTO.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace AlkemyWallet.Core.Models.DTO;
+
+public class UserDTO
+{
+    [Column("first_name")]
+    public string FirstName { get; set; }
+
+    [Column("last_name")]
+    public string LastName { get; set; }
+
+    [Column("email")]
+    [EmailAddress]
+    [Required]
+    public string Email { get; set; }
+
+    [Column("password")]
+    [Required]
+    public string Password { get; set; }
+
+    [Column("points")]
+    public int? Points { get; set; }
+
+    [Column("rol_Id")]
+    [ForeignKey("Role")]
+    public int RoleId { get; set; }
+}
