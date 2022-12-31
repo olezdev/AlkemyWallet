d7e22aae22b47cb4aeabb869bc3ed6422ab8c362
Olivera Leonardo <oliveraleonardoezequiel@gmail.com>
2022-12-30 21:21:51 -0300

    Add TransactionControllerGet

---
+++ AlkemyWallet/Controllers/TransactionsController.cs
@@ -0,0 +1,24 @@
+﻿using AlkemyWallet.Core.Models.DTO;
+using AlkemyWallet.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlkemyWallet.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class TransactionsController : ControllerBase
+{
+    private readonly ITransactionService _transactionService;
+
+	public TransactionsController(ITransactionService transactionService)
+	{
+		_transactionService = transactionService;
+	}
+
+	[HttpGet]
+	public async Task<IActionResult> Get()
+	{
+		return Ok(await _transactionService.GetAllAsync());
+	}
+}
