d16d9bb74629c2c7f1ca5951ef5094340c4b6579
Olivera Leonardo <oliveraleonardoezequiel@gmail.com>
2023-01-17 18:15:43 -0300

    Add AccountsPATCH isBlocked

---
+++ AlkemyWallet/Core/Models/DTO/AccountUpdatedDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace AlkemyWallet.Core.Models.DTO;
+
+public class AccountUpdatedDTO
+{
+    public int UserId { get; set; }
+    public DateTime CreationDate { get; set; }
+    public bool IsBlocked { get; set; }
+}
--- AlkemyWallet/Services/Interfaces/IAccountService.cs
+++ AlkemyWallet/Services/Interfaces/IAccountService.cs
@@ -7,4 +7,5 @@ public interface IAccountService
     Task<List<AccountsDTO>> GetAllAsync();
     Task<AccountDetailsDTO> GetByIdAsync(int id);
     Task<AccountCreatedDTO> CreateAsync(int userId);
+    Task<AccountUpdatedDTO> UpdateBlockedAsync(int id, AccountToUpdateDTO accountDTO);
 }
