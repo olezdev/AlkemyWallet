76d8aabc591b97479b53907a3028f00cbff7dddc
Leonardo Olivera <olezdev@gmail.com>
2023-01-27 15:41:47 -0300

    Add Accounts Documentation

--- AlkemyWallet/Controllers/AccountsController.cs
+++ AlkemyWallet/Controllers/AccountsController.cs
@@ -1,11 +1,10 @@
-﻿using AlkemyWallet.Core.Filters;
-using AlkemyWallet.Core.Models.DTO;
-using AlkemyWallet.Services.Interfaces;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Net.Http;
-using Microsoft.AspNetCore.Hosting;
+using AlkemyWallet.Services.Interfaces;
+using AlkemyWallet.Core.Filters;
+using AlkemyWallet.Core.Models.DTO;
 using AlkemyWallet.Core.Helper;
+using AlkemyWallet.Entities;
 
 namespace AlkemyWallet.Controllers;
 
@@ -33,6 +32,24 @@ public class AccountsController : ControllerBase
     //    return Ok(accounts);
     //}
 
+    /// <summary>
+    /// Paged list of all accounts. Only available for Administrators.
+    /// </summary>
+    /// <remarks>
+    /// Sample request:
+    /// 
+    ///     GET /accounts?page=1
+    ///     
+    /// </remarks>
+    /// <returns>The list of Accounts</returns>
+    /// <response code="200">All Accounts</response>
+    /// <response code="401">Invalid authentication credentials for the requested resource</response>
+    /// <response code="403">User has not permission</response>
+    /// <response code="404">Accounts not found</response>
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<TransactionsDTO>))]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     [HttpGet]
     [Authorize(Roles = "Admin")]
     public async Task<IActionResult> GetAll([FromQuery] PaginationFilter filter)
@@ -53,6 +70,26 @@ public class AccountsController : ControllerBase
 
     }
 
+    /// <summary>
+    /// Get an account. Only available for Admin.
+    /// </summary>
+    /// <remarks>
+    /// Sample request:
+    /// 
+    ///     GET /accounts/1
+    ///     
+    /// </remarks>
+    /// <param name="id">Account Id</param> 
+    /// <returns>Account</returns>
+    /// <response code="200">Return an account</response>
+    /// <response code="401">Invalid authentication credentials for the requested resource</response>
+    /// <response code="403">User has not permission</response>
+    /// <response code="404">Account not found</response>
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(AccountDetailsDTO))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     [HttpGet("{id}")]
     [Authorize(Roles = "Admin")]
     public async Task<IActionResult> GetById(int id)
@@ -64,6 +101,24 @@ public class AccountsController : ControllerBase
         return Ok(account);
     }
 
+    /// <summary>
+    /// Create account.
+    /// </summary>
+    /// <remarks>
+    /// Sample request:
+    /// 
+    ///     POST /accounts
+    ///     
+    /// </remarks>
+    /// <returns>Created Account</returns>
+    /// <response code="200">A created account</response>
+    /// <response code="400">If the item is null</response>
+    /// <response code="401">Invalid authentication credentials for the requested resource</response>
+    /// <response code="403">User has not permission</response>
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(AccountCreatedDTO))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
     [HttpPost]
     [Authorize(Roles = "Regular")]
     public async Task<IActionResult> Post()
@@ -77,6 +132,28 @@ public class AccountsController : ControllerBase
         return Ok(account);
     }
 
+    /// <summary>
+    /// Update an existing Account.
+    /// </summary>
+    /// <remarks>
+    /// Sample request:
+    /// 
+    ///     PATCH /accounts/1
+    ///     {
+    ///       "isBlocked": "test"
+    ///     }
+    ///     
+    /// </remarks>
+    /// <param name="id"></param>
+    /// <param name="accountDTO"></param>
+    /// <returns>IsBlocked Updated</returns>
+    /// <response code="200">Account was successfully updated.</response>
+    /// <response code="400">Information for update it is not valid</response>
+    /// <response code="401">Invalid authentication credentials for the requested resource</response>
+    /// <response code="403">User has not permission.</response>
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(AccountUpdatedDTO))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
     [HttpPatch("{id}")]
     [Authorize(Roles = "Admin")]
     public async Task<IActionResult> UpdateBlocked(int id, AccountToUpdateDTO accountDTO)
@@ -87,6 +164,24 @@ public class AccountsController : ControllerBase
         return Ok(account);
     }
 
+    /// <summary>
+    /// Delete an existing Account.
+    /// </summary>
+    /// <remarks>
+    /// Sample request:
+    /// 
+    ///     DELETE /accounts/1
+    ///     
+    /// </remarks>
+    /// <param name="id"></param>
+    /// <returns>NoContent</returns>
+    /// <response code="200">Account was successfully deleted.</response>
+    /// <response code="400">Information for delete it is not valid.</response>
+    /// <response code="401">Invalid authentication credentials for the requested resource.</response>
+    /// <response code="403">User has not permission.</response>
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
     [HttpDelete("{id}")]
     [Authorize(Roles = "Admin")]
     public async Task<IActionResult> Delete(int id)
@@ -98,6 +193,38 @@ public class AccountsController : ControllerBase
         return NoContent();
     }
 
+    /// <summary>
+    /// Update Account and Create Deposit or Transfer Transaction .
+    /// </summary>
+    /// <remarks>
+    /// Sample request:
+    /// 
+    ///     POST /accounts/1
+    ///     {
+    ///       "Amount": "1000",
+    ///       "Type": "Deposito",
+    ///       "Concept": "test",
+    ///       "ToAccountId": "1"
+    ///     }
+    ///     
+    ///     POST /accounts/1
+    ///     {
+    ///       "Amount": "500",
+    ///       "Type": "Transferencia",
+    ///       "Concept": "test",
+    ///       "ToAccountId": "2"
+    ///     }
+    ///     
+    /// </remarks>
+    /// <returns>Created Account</returns>
+    /// <response code="200">A created account</response>
+    /// <response code="400">If the item is null</response>
+    /// <response code="401">Invalid authentication credentials for the requested resource</response>
+    /// <response code="403">User has not permission</response>
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(TransactionDTO))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
     [HttpPost("{id}")]
     [Authorize(Roles = "Regular")]
     public async Task<IActionResult> TransactionAsync(int id, TransactionDTO transactionDTO)
